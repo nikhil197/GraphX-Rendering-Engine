c26c72ad352d96b4fd116342aa3368e268c4e53d
Mohit <singhmohit9797@gmail.com>
2018-10-31 15:34:56 +0530

    Completed Matrix3.h and Matrix3.cpp

--- GraphXM/src/matrices/Matrix3.h
+++ GraphXM/src/matrices/Matrix3.h
@@ -20,12 +20,82 @@ namespace gm
 		Matrix3(const Matrix3& OtherMat);
 
 	public:
+		/* Returns whether the matrix is equal to this one */
+		bool operator==(const Matrix3& OtherMat) const;
+
+		/* Returns whether the matrix is not equal to this one */
+		bool operator!=(const Matrix3& OtherMat) const;
+
+		/* Returns a matrix obtained after adding the matrix to this one */
+		const Matrix3 operator+(const Matrix3& OtherMat) const;
+
+		/* Returns a matrix obtained after subtracting the matrix from this one */
+		const Matrix3 operator-(const Matrix3& OtherMat) const;
+
+		/* Returns a matrix after multiplying the matrix with this one */
+		const Matrix3 operator*(const Matrix3& OtherMat) const;
+
+		/* Returns the matrix obtained after adding value to this matrix */
+		const Matrix3 operator+(float Value) const;
+
+		/* Returns the matrix obtained after subtracting value from this matrix */
+		const Matrix3 operator-(float Value) const;
+
+		/* Returns the matrix obtained after multiplying value with this matrix */
+		const Matrix3 operator*(float Value) const;
+
+		/* Returns the matrix obtained after dividing this matrix with value */
+		const Matrix3 operator/(float Value) const;
+
+		/********* Assignment operators ***********/
+
+		/* Adds the matrix to this matrix */
+		Matrix3& operator+=(const Matrix3& OtherMat);
+
+		/* Subtracts the matrix to this matrix */
+		Matrix3& operator-=(const Matrix3& OtherMat);
+
+		/* Multiply matrix to this matrix */
+		Matrix3& operator*=(const Matrix3& OtherMat);
+
+		/* Adds the value to this matrix */
+		Matrix3& operator+=(float Value);
+
+		/* Subtracts the value from this matrix */
+		Matrix3& operator-=(float Value);
+
+		/* Multiplies value with this matrix */
+		Matrix3& operator*=(float Value);
+
+		/* Divides the matrix with value */
+		Matrix3& operator/=(float Value);
+
+	public:
+		/* Returns the vector obtained after multipying a vec4 with this matrix */
+		const Vector3 operator*(const Vector3& Vec) const;
+
+	public:
 		/* Converts this matrix into an identiy matrix */
 		void Identity();
+
+		/* Returns a transpose matrix of this one */
+		Matrix3 Transpose() const;
+
+		/* Returns the determinant of this matrix */
+		float Determinant() const;
+
+		/* Returns a matrix that is the adjoint of this matrix */
+		Matrix3 Adjoint() const;
+
+		/* Returns an inverse matrix of this matrix */
+		Matrix3 Inverse() const;
 
 	private:
 		/* Initialise the matrix */
 		void Init(float Value);
+
+		/* Initialise the matrix with the contents of an array */
+		void Init(const float(*arr)[3]);
 
 	};
 }
