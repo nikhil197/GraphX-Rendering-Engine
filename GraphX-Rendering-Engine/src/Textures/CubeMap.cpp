b30a11ab61319b67a64e8709d66111092151a226
Nikhil Bansal <nikhil.bansal97@outlook.com>
2018-12-30 13:42:37 +0530

    Added cubemap textures

---
+++ GraphX-Rendering-Engine/src/Textures/CubeMap.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+
+#include "CubeMap.h"
+#include "stb/stb_image.h"
+
+namespace engine
+{
+	CubeMap::CubeMap(const std::string& FilePath, const std::vector<std::string>& FileNames)
+		: m_FilePath(FilePath), m_FileNames(FileNames), m_LocalBuffer(nullptr)
+	{
+		// To flip the textures on load
+		stbi_set_flip_vertically_on_load(1);
+
+		GLCall(glGenTextures(1, &m_RendererID));
+		GLCall(glBindTexture(GL_TEXTURE_CUBE_MAP, m_RendererID));
+
+		// Specify the parameters for texture wrapping
+		GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+		GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+		GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE));
+
+		// Specify the parameters for texture filtering (min - max)
+		GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+		GLCall(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+
+		for (unsigned int i = 0; i < m_FileNames.size(); i++)
+		{
+			m_LocalBuffer = stbi_load((m_FilePath + m_FileNames[i]).c_str(), &m_Width, &m_Height, &m_BPP, 4 /*RGBA*/);
+
+			GLCall(glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_LocalBuffer));
+
+			// Free the texture data
+			stbi_image_free(m_LocalBuffer);
+		}
+
+		UnBind();
+	}
+
+	void CubeMap::Bind(unsigned int slot) const
+	{
+		GLCall(glActiveTexture(GL_TEXTURE0 + slot));
+		GLCall(glBindTexture(GL_TEXTURE_CUBE_MAP, m_RendererID));
+	}
+
+	void CubeMap::UnBind() const
+	{
+		GLCall(glBindTexture(GL_TEXTURE_CUBE_MAP, 0));
+	}
+
+	CubeMap::~CubeMap()
+	{
+		GLCall(glDeleteTextures(1, &m_RendererID));
+	}
+}
---
+++ GraphX-Rendering-Engine/src/Textures/CubeMap.h
@@ -0,0 +1,48 @@
+#pragma once
+
+namespace engine
+{
+	class CubeMap
+	{
+	private:
+		/* ID for the cubemap used by the renderer */
+		unsigned int m_RendererID;
+
+		/* Path to the cubemap texture */
+		std::string m_FilePath;
+
+		/* Names of the 6 textures located at the path specified in the file path */
+		std::vector<std::string> m_FileNames;
+
+		/* Local buffer to store the pixel data */
+		unsigned char* m_LocalBuffer;
+
+		/* Width and Height for each texture */
+		int m_Width, m_Height;
+
+		/* Bits per pixel for each texture */
+		int m_BPP;
+
+	public:
+		/*
+		*	Creates a cubemap
+		* @param FilePath Path to the folder containing the textures to be used to draw the cubemap
+		* @param FileNames names of the files to be used as textures for the six faces of the cubemap. Order of the names should be right, left, top, bottom, front, back faces
+		*/
+		CubeMap(const std::string& FilePath, const std::vector<std::string>& FileNames);
+
+		/* Bind the cubemap to specified slot */
+		void Bind(unsigned int slot = 0) const;
+
+		/* Unbind the currenlty bound cubemap */
+		void UnBind() const;
+
+		/* Returns the width of the texture */
+		inline int GetWidth() const { return m_Width; }
+
+		/* Returns the height of the texture */
+		inline int GetHeight() const { return m_Height; }
+
+		~CubeMap();
+	};
+}
