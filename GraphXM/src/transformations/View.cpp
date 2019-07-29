9b94d54bee79cef63acc04d04d2975cf1c66183b
Nikhil Bansal <nikhil.bansal97@outlook.com>
2019-07-30 00:51:49 +0530

    View Transformation Matrix optimisations

--- GraphXM/src/transformations/View.cpp
+++ GraphXM/src/transformations/View.cpp
@@ -10,6 +10,22 @@ namespace gm
 	{
 		Matrix4 result;
 		
+		LookAt(result, CameraPosition, LookAtPoint, UpVector);
+
+		return result;
+	}
+
+	Matrix4 View::LookAt(const Vector3& CameraPosition, const Vector3& ViewAxis, const Vector3& RightAxis, const Vector3& UpAxis)
+	{
+		Matrix4 result;
+
+		LookAt(result, CameraPosition, ViewAxis, RightAxis, UpAxis);
+
+		return result;
+	}
+
+	void View::LookAt(Matrix4& Mat, const Vector3& CameraPosition, const Vector3& LookAtPoint, const Vector3& UpVector)
+	{
 		// New z - axis after the camera transformations (View Vector i.e. from target to camera)
 		Vector3 ViewVector = (LookAtPoint - CameraPosition);
 		ViewVector.Normalize();
@@ -21,24 +37,28 @@ namespace gm
 		// New y - axis (Final Up Vector)
 		Vector3 NewUpVector = Vector3::CrossProduct(RightVector, ViewVector);
 
-		result[0][0] = RightVector.x;
-		result[0][1] = RightVector.y;
-		result[0][2] = RightVector.z;
+		LookAt(Mat, CameraPosition, ViewVector, RightVector, NewUpVector);
+	}
 
-		result[1][0] = NewUpVector.x;
-		result[1][1] = NewUpVector.y;
-		result[1][2] = NewUpVector.z;
+	void View::LookAt(Matrix4& Mat, const Vector3& CameraPosition, const Vector3& ViewAxis, const Vector3& RightAxis, const Vector3& UpAxis)
+	{
+		Mat[0][0] = RightAxis.x;
+		Mat[0][1] = RightAxis.y;
+		Mat[0][2] = RightAxis.z;
 
-		result[2][0] = -ViewVector.x;
-		result[2][1] = -ViewVector.y;
-		result[2][2] = -ViewVector.z;
+		Mat[1][0] = UpAxis.x;
+		Mat[1][1] = UpAxis.y;
+		Mat[1][2] = UpAxis.z;
+
+		Mat[2][0] = -ViewAxis.x;
+		Mat[2][1] = -ViewAxis.y;
+		Mat[2][2] = -ViewAxis.z;
 
 		// Get the projection of the translation vector onto the new axes
-		result[0][3] = -Vector3::DotProduct(RightVector, CameraPosition);
-		result[1][3] = -Vector3::DotProduct(NewUpVector, CameraPosition);
-		result[2][3] =  Vector3::DotProduct(ViewVector, CameraPosition);
+		Mat[0][3] = -Vector3::DotProduct(RightAxis, CameraPosition);
+		Mat[1][3] = -Vector3::DotProduct(UpAxis, CameraPosition);
+		Mat[2][3] = Vector3::DotProduct(ViewAxis, CameraPosition);
 		//Translation trans(-CameraPosition);
-
-		return result;
 	}
+
 }
--- GraphXM/src/transformations/View.h
+++ GraphXM/src/transformations/View.h
@@ -14,7 +14,15 @@ namespace gm
 		View();
 
 	public:
-		/* Returns a view matrix representing a camera at CameraPosition, focussed at LookAtPoint with UpVector as up direction (This matrix is considers the right handed coordinate system i.e. outward is +ve z-axis) */
+		/* Returns a view matrix representing a camera at CameraPosition, focused at LookAtPoint with UpVector as up direction (This matrix is considers the right handed coordinate system i.e. outward is +ve z-axis) */
 		static Matrix4 LookAt(const Vector3& CameraPosition, const Vector3& LookAtPoint, const Vector3& UpVector);
+
+		/* Returns a view matrix representing camera at CameraPosition and orientation given by the axes */
+		static Matrix4 LookAt(const Vector3& CameraPosition, const Vector3& ViewAxis, const Vector3& RightAxis, const Vector3& UpAxis);
+
+		static void LookAt(Matrix4& Mat, const Vector3& CameraPosition, const Vector3& LookAtPoint, const Vector3& UpVector);
+
+		static void LookAt(Matrix4& Mat, const Vector3& CameraPosition, const Vector3& ViewAxis, const Vector3& RightAxis, const Vector3& UpAxis);
+
 	};
 }
