55763235652418534b6d357bf3a5b50046796e46
Nikhil Bansal <nikhil.bansal97@outlook.com>
2020-05-20 03:51:08 +0530

    Updated project config

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,18 +1,7 @@
-
-//#include "pch.h"
 #include "GraphX.h"
 #include "EntryPoint.h"
 
-class SandboxApp
-	: public GraphX::Application
-{
-public:
-	SandboxApp()
-		: Application("GraphX-Rendering-Engine", 1920, 1080)
-	{
-
-	}
-};
+#include "SandboxApp.h"
 
 GraphX::Application* GraphX::CreateApplication()
 {
