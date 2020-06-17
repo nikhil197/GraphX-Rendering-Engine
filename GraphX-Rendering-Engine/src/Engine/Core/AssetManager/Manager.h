53618297677354136ea41a48e15c50dc8e2fb19f
Nikhil Bansal <nikhil.bansal97@outlook.com>
2020-06-17 13:42:24 +0530

    Bug Fix: Passing arguments by value when creating Refs

--- GraphX-Rendering-Engine/src/Engine/Core/AssetManager/Manager.h
+++ GraphX-Rendering-Engine/src/Engine/Core/AssetManager/Manager.h
@@ -19,7 +19,7 @@ namespace GraphX
 	using Ref = std::shared_ptr<T>;
 
 	template<typename T, typename ... Args>
-	constexpr Ref<T> CreateRef(Args ... args)
+	constexpr Ref<T> CreateRef(Args&& ... args)
 	{
 		return std::make_shared<T>(std::forward<Args>(args)...);
 	}
