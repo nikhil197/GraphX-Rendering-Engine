8f3bff5a6165b779375c69a13847a8e608c92c9a
Nikhil Bansal <nikhil.bansal97@outlook.com>
2021-01-11 00:25:50 +0530

    Fixed ImGui Bugs - Window close not working properly

--- GraphX-Rendering-Engine/src/Engine/Gui/GraphXGui.cpp
+++ GraphX-Rendering-Engine/src/Engine/Gui/GraphXGui.cpp
@@ -56,38 +56,43 @@ namespace GraphX
 	void GraphXGui::GlobalSettings(const Ref<Skybox>& skybox, float& daytime, float& SunLightIntensity, bool& EnableParticles)
 	{
 		static bool ShowGlobalSettingsWindow = true;
-		ImGui::Begin("Global Settings", &ShowGlobalSettingsWindow);
+		if (ShowGlobalSettingsWindow)
+		{
+			ImGui::Begin("Global Settings", &ShowGlobalSettingsWindow);
 
-		ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Skybox Details");
-		ImGui::DragFloat("Rotation Speed", &skybox->RotationSpeed, 0.1f, 0.0f, 2.0f);
+			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Skybox Details");
+			ImGui::DragFloat("Rotation Speed", &skybox->RotationSpeed, 0.1f, 0.0f, 2.0f);
 
-		ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Day Time Details");
-		ImGui::DragFloat("Day Time Hours", &daytime, 0.1f, 0.0f, 2.0f);
+			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Day Time Details");
+			ImGui::DragFloat("Day Time Hours", &daytime, 0.1f, 0.0f, 2.0f);
 
-		ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Global Light Intensity");
-		ImGui::DragFloat("Global Light Intensity", &SunLightIntensity, 0.1f, 0.1f, 5.0f);
+			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Global Light Intensity");
+			ImGui::DragFloat("Global Light Intensity", &SunLightIntensity, 0.1f, 0.1f, 5.0f);
 
-		ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Enable Particle Effects");
-		ImGui::Checkbox("Enable Particle Effects", &EnableParticles);
-		ImGui::End();
+			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Enable Particle Effects");
+			ImGui::Checkbox("Enable Particle Effects", &EnableParticles);
+			ImGui::End();
+		}
 	}
 
 	void GraphXGui::DetailsWindow(const Ref<Mesh3D>& mesh, const std::string& Name)
 	{
+		static bool ShowDetailsWindow = true;
 		if (mesh->bShowDetails)
 		{
-			ImGui::Begin(Name.c_str(), (bool*)mesh->bShowDetails);
+			ShowDetailsWindow = mesh->bShowDetails;
+			ImGui::Begin(Name.c_str(), &ShowDetailsWindow);
 			ImGui::Spacing();
 			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Transformations");
 			if (ImGui::DragFloat3("Translation", &mesh->Position.x, 1.0f, -1000.0f, 1000.0f))
 			{
 				mesh->UpdateModelMatrix(true);
 			}
-			if (ImGui::DragFloat3("Rotation", (float*)& mesh->Rotation, 1.0f, -359.0f, 359.f))
+			if (ImGui::DragFloat3("Rotation", (float*)&mesh->Rotation, 1.0f, -359.0f, 359.f))
 			{
 				mesh->UpdateModelMatrix(true);
 			}
-			if (ImGui::DragFloat3("Scaling", (float*)& mesh->Scale.x, 1.0f, 0.0001f, 10.f))
+			if (ImGui::DragFloat3("Scaling", (float*)&mesh->Scale.x, 1.0f, 0.0001f, 10.f))
 			{
 				mesh->UpdateModelMatrix(true);
 			}
@@ -102,17 +107,17 @@ namespace GraphX
 			float Reflectivity = Mat->GetSpecularStrength();
 			float Shininess = Mat->GetShininess();
 
-			if (ImGui::DragFloat("Shininess", (float*)& Shininess, 1.0f, 2.0f, 256.0f))
+			if (ImGui::DragFloat("Shininess", (float*)(&Shininess), 1.0f, 2.0f, 256.0f))
 			{
 				Mat->SetShininess(Shininess);
 			}
 
-			if (ImGui::DragFloat("Reflectivity", (float*)& Reflectivity, 1.0f, 0.0f, 1.0f))
+			if (ImGui::DragFloat("Reflectivity", (float*)(&Reflectivity), 1.0f, 0.0f, 1.0f))
 			{
 				Mat->SetSpecularStrength(Reflectivity);
 			}
 
-			if (ImGui::ColorEdit4("Tint Color", (float*)& BaseColor))
+			if (ImGui::ColorEdit4("Tint Color", (float*)(&BaseColor)))
 			{
 				Mat->SetBaseColor(BaseColor);
 			}
@@ -128,7 +133,7 @@ namespace GraphX
 			{
 				ImGui::Text("%s", Textures.at(n)->GetFilePath().c_str());
 			}
-			if(ImGui::Button("Add New Texture"))
+			if (ImGui::Button("Add New Texture"))
 			{
 				// Add a new texture to the mesh
 				if (s_GuiEventCallback)
@@ -139,14 +144,17 @@ namespace GraphX
 			}
 			ImGui::EndChild();
 			ImGui::End();
+			mesh->bShowDetails = ShowDetailsWindow;
 		}
 	}
 
 	void GraphXGui::DetailsWindow(const Ref<Mesh2D>& mesh)
 	{
+		static bool ShowDetailsWindow = true;
 		if (mesh->bShowDetails)
 		{
-			ImGui::Begin("Details", (bool*)mesh->bShowDetails);
+			ShowDetailsWindow = mesh->bShowDetails;
+			ImGui::Begin("Details", &ShowDetailsWindow);
 
 			ImGui::Text("Transformations");
 			ImGui::SliderFloat3("Translation", (float*)&mesh->Position.x, -1000.0f, 1000.0f);
@@ -176,103 +184,112 @@ namespace GraphX
 			}
 
 			ImGui::End();
+			mesh->bShowDetails = ShowDetailsWindow;
 		}
 	}
 
 	void GraphXGui::LightProperties(const Ref<PointLight>& light)
 	{
+		static bool ShowLightProperties = true;
 		if (light->bShowDetails)
 		{
-			ImGui::Begin("Lighting", (bool*)light->bShowDetails);
+			ShowLightProperties = light->bShowDetails;
+			ImGui::Begin("Lighting", &ShowLightProperties);
 			ImGui::DragFloat3("Position", (float*)&light->Position.x, 1.0f, -500.0f, 500.0f);
 			ImGui::DragFloat("Intensity", (float*)&light->Intensity, 1.0f, 0.0f, 100.0f);
 			ImGui::ColorEdit4("Color", (float*)&light->Color);
 			ImGui::End();
+			light->bShowDetails = ShowLightProperties;
 		}
 	}
 
 	void GraphXGui::CameraProperties(const Ref<CameraController>& cameraController)
 	{
 		static bool ShowCameraPropertiesWindow = true;
-
-		float FOV = cameraController->GetFieldOfView();
-		bool bPerspectiveMode = cameraController->GetProjectionMode() == ProjectionMode::Perspective;
-		ImGui::Begin("Camera Properties", &ShowCameraPropertiesWindow);
-		ImGui::DragFloat("Camera Translation Speed", (float*)&cameraController->TranslationSpeed, 1.0f, 0.0f, 100.0f);
-		ImGui::DragFloat("Camera Roll Speed", (float*)&cameraController->RollSpeed, 1.0f, 30.0f, 360.0f);
-
-		if (ImGui::DragFloat("Field Of View", &FOV, .1f, 10.0f, 100.0f))
+		if (ShowCameraPropertiesWindow)
 		{
-			if (s_GuiEventCallback)
-			{
-				CameraFOVChangedEvent e(*cameraController->GetCamera(), FOV);
-				s_GuiEventCallback(e);
-			}
-		}
-		
-		ImGui::DragFloat("Mouse Sensitivity", (float*)&Mouse::GetMouse()->Sensitivity, 0.01f, 0.5f, 2.5f);
+			float FOV = cameraController->GetFieldOfView();
+			bool bPerspectiveMode = cameraController->GetProjectionMode() == ProjectionMode::Perspective;
+			ImGui::Begin("Camera Properties", &ShowCameraPropertiesWindow);
+			ImGui::DragFloat("Camera Translation Speed", (float*)&cameraController->TranslationSpeed, 1.0f, 0.0f, 100.0f);
+			ImGui::DragFloat("Camera Roll Speed", (float*)&cameraController->RollSpeed, 1.0f, 30.0f, 360.0f);
 
-		if (ImGui::RadioButton("Perspective", bPerspectiveMode))
-		{
-			if (s_GuiEventCallback)
+			if (ImGui::DragFloat("Field Of View", &FOV, .1f, 10.0f, 100.0f))
 			{
-				CameraProjectionModeChange e(*cameraController->GetCamera(), ProjectionMode::Perspective);
-				s_GuiEventCallback(e);
+				if (s_GuiEventCallback)
+				{
+					CameraFOVChangedEvent e(*cameraController->GetCamera(), FOV);
+					s_GuiEventCallback(e);
+				}
 			}
-		}
-		ImGui::SameLine();
-		if (ImGui::RadioButton("Orthographic", !bPerspectiveMode))
-		{
-			if (s_GuiEventCallback)
+
+			ImGui::DragFloat("Mouse Sensitivity", (float*)&Mouse::GetMouse()->Sensitivity, 0.01f, 0.5f, 2.5f);
+
+			if (ImGui::RadioButton("Perspective", bPerspectiveMode))
 			{
-				CameraProjectionModeChange e(*cameraController->GetCamera(), ProjectionMode::Orthographic);
-				s_GuiEventCallback(e);
+				if (s_GuiEventCallback)
+				{
+					CameraProjectionModeChange e(*cameraController->GetCamera(), ProjectionMode::Perspective);
+					s_GuiEventCallback(e);
+				}
 			}
+			ImGui::SameLine();
+			if (ImGui::RadioButton("Orthographic", !bPerspectiveMode))
+			{
+				if (s_GuiEventCallback)
+				{
+					CameraProjectionModeChange e(*cameraController->GetCamera(), ProjectionMode::Orthographic);
+					s_GuiEventCallback(e);
+				}
+			}
+			ImGui::End();
 		}
-		ImGui::End();
 	}
 
 	void GraphXGui::TerrainDetails(const Ref<Terrain>& terrain)
 	{
 		static bool ShowTerrainDetailsWindow = true;
-
-		ImGui::Begin("Terrain Details", &ShowTerrainDetailsWindow);
-		if (ImGui::DragFloat3("Position", (float*)&terrain->GetMesh()->Position.x, 1.0f, -1000.0f, 1000.0f))
+		if (ShowTerrainDetailsWindow)
 		{
-			Mesh3D& mesh = const_cast<Mesh3D&>(*terrain->GetMesh());
-			mesh.UpdateModelMatrix(true);
+			ImGui::Begin("Terrain Details", &ShowTerrainDetailsWindow);
+			if (ImGui::DragFloat3("Position", (float*)&terrain->GetMesh()->Position.x, 1.0f, -1000.0f, 1000.0f))
+			{
+				terrain->GetMesh()->UpdateModelMatrix(true);
+			}
+			ImGui::DragFloat3("Scale in X & Z", (float*)&terrain->GetMesh()->Scale.x, 1.0f, 0.0001f, 10.f);
+			ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Note: Scaling in y - direction is not advised ");
+			ImGui::End();
 		}
-		ImGui::DragFloat3("Scale in X & Z", (float*)&terrain->GetMesh()->Scale.x, 1.0f, 0.0001f, 10.f);
-		ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "Note: Scaling in y - direction is not advised ");
-		ImGui::End();
 	}
 
 	void GraphXGui::Models()
 	{
 		static bool ShowModelsWindow = true;
-
-		ImGui::Begin("Models", &ShowModelsWindow);
-		if(ImGui::Button("Cube"))
+		if (ShowModelsWindow)
 		{
-			// Add Cube to the scene
-			if (s_GuiEventCallback)
+			ImGui::Begin("Models", &ShowModelsWindow);
+			if (ImGui::Button("Cube"))
 			{
-				AddModelEvent e(ModelType::CUBE);
-				s_GuiEventCallback(e);
+				// Add Cube to the scene
+				if (s_GuiEventCallback)
+				{
+					AddModelEvent e(ModelType::CUBE);
+					s_GuiEventCallback(e);
+				}
 			}
-		}
-		if (ImGui::Button("Add Model"))
-		{
-			// Load a new, custom Model from the file system
-			if (s_GuiEventCallback)
+			if (ImGui::Button("Add Model"))
 			{
-				AddModelEvent e(ModelType::CUSTOM);
-				s_GuiEventCallback(e);
+				// Load a new, custom Model from the file system
+				if (s_GuiEventCallback)
+				{
+					AddModelEvent e(ModelType::CUSTOM);
+					s_GuiEventCallback(e);
+				}
 			}
-		}
-		AddTerrain();
+			AddTerrain();
 
-		ImGui::End();
+			ImGui::End();
+		}
 	}
 
 	void GraphXGui::AddTerrain()
